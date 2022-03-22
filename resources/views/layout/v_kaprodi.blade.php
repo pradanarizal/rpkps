676c8c4c505abf5ced0eafc4392cc1c3dda19ead
pradanarizal <rizalazizpradana1@gmail.com>
2022-03-22 23:26:55 +0700

    profil

--- resources/views/layout/v_kaprodi.blade.php
+++ resources/views/layout/v_kaprodi.blade.php
@@ -56,7 +56,7 @@
                 <!-- Sidebar user panel -->
                 <div class="user-panel">
                     <div class="pull-left image">
-                        <img src="{{asset('adminlte')}}/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                        <img src="{{asset('gambar')}}/gbr1.jpg" class="img-circle" alt="User Image">
                     </div>
                     <div class="pull-left info">
                         <p>Rizal Pradana</p>
