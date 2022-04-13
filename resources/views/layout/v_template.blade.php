bdde6ae159c60f9cef0be9c92a32ae5fff5ec1e6
pradanarizal <rizalazizpradana1@gmail.com>
2022-04-13 10:19:21 +0700

    rizal

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <!-- tes -->
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,6 +23,7 @@
     <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
+<!-- rizal -->
 
 <body class="hold-transition skin-black sidebar-mini">
     <!-- Site wrapper -->
